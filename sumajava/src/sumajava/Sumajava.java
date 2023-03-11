4a602fd7172a6254688c90f4bf641b4c7944863f
amelia588 <1473967@senati.pe>
2023-03-11 11:02:41 -0500

    ejercio en java

---
+++ sumajava/src/sumajava/Sumajava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumajava;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Sumajava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int 1=1;
+        int numero=0;
+        do {
+            System.out.println("+"+1);
+            numero=numero+1;
+            1++;
+        } while (1<=7);
+        System.out.println("+"+ numero);
+    
+    
+}
